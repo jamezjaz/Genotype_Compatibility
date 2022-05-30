20d952a113804316baad70fe862c01386d3d0a72
Jamezjaz <jamezjaz@gmail.com>
2022-05-30 19:57:01 +0100

    feat: Adds Genotype class

---
+++ Genotype.java
@@ -0,0 +1,6 @@
+public class Genotype {
+    public static void main(String[] args) {
+        System.out.println("\n" +
+                "Two people are said to have compatible genotypes when there is no possibility of them birthing a child with sickle cell disease");
+    }
+}
---
+++ out/production/Genotype_Compatibility/README.md
@@ -0,0 +1,2 @@
+# Genotype_Compatibility
+This is a GUI program built with Java. It tells users whether or not they're compatible with their partners. The program informs them whether they're safe to marry their partners or not.
