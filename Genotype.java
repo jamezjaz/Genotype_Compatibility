7ed00e0046d66e871abeee0ef091b757fde33d8c
Jamezjaz <jamezjaz@gmail.com>
2022-05-30 20:16:01 +0100

    feat: Adds program description

--- Genotype.java
+++ Genotype.java
@@ -1,6 +1,7 @@
+import javax.swing.*;
+
 public class Genotype {
-    public static void main(String[] args) {
-        System.out.println("\n" +
-                "Two people are said to have compatible genotypes when there is no possibility of them birthing a child with sickle cell disease");
+    public static  void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "★★★GENOTYPE COMPATIBILITY★★★" + "\n" + "This is a genotype compatibility program. You'll get to understand whether or not you're compatible with your partner(s)." + "\n" + "Enjoy!");
     }
 }
